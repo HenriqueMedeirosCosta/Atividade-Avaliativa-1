4401fe664f9310401226fea586671628f2f53cae
HenriqueMedeirosCosta <132111448+HenriqueMedeirosCosta@users.noreply.github.com>
2023-10-01 17:47:16 -0300

    Create exercício3.c

---
+++ exercício3.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main() {
+  int a, b, c, triangulo;
+
+  printf("Digite o valor de A\n");
+  scanf("%d,", &a);
+  printf("Digite o valor de B\n");
+  scanf("%d", &b);
+  printf("Digite o valor de C\n");
+  scanf("%d", &c);
+
+  if ((a + b) > c && (c + a) > b && (b + c) > a) {
+    if (a == b && b == c) {
+      printf("Esses valores formam um triângulo equilátero");
+    } else if (a == b || b == c || c == a) {
+      printf("Esses valores formam um triângulo isósceles");
+    } else if (a != b && a != c && b != c) {
+      printf("Esses valores formam um triângulo escaleno");
+    }
+  } else {
+    printf("Esses valores não formam um triangulo.");
+  }
